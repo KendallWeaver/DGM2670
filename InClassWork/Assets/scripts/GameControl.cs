f917a18e23460505fa97659979f8f52823c5835c
Unknown <flaze5@gmail.com>
2016-10-17 13:58:08 -0600

    Oct 17

--- InClassWork/Assets/scripts/GameControl.cs
+++ InClassWork/Assets/scripts/GameControl.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class GameControl : MonoBehaviour {
 
-   
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }
---
+++ InClassWork/Assets/scripts/Gravity.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+    // Once again I came in late... :(
+    CharacterController myCC;
+    Vector3 tempPos;
+    public float gravity = 1;
+
+    
+    public float sideForce = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myCC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos.y -= gravity * Time.deltaTime;
+
+        tempPos.z = sideForce;
+
+        if (myCC.isGrounded)
+        {
+            tempPos.y = 0;
+        }
+        print(myCC.velocity);
+        myCC.Move(tempPos);
+    }
+}
