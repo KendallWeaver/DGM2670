80378e6ec7da9b5047c3b1d9cdaf8f76ee5115ef
Unknown <flaze5@gmail.com>
2016-10-19 13:55:37 -0600

    Oct 19

---
+++ InClassWork/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    // came in late (cause of work), missed the script cause VISUAL STUDIOS REFUSED TO LOAD UP Dx
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
