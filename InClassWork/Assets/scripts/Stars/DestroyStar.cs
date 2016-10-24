3af39d2a40cdbefbb337ce2445533db0f88141ba
Unknown <flaze5@gmail.com>
2016-10-24 13:51:58 -0600

    Oct 24

---
+++ InClassWork/Assets/scripts/Stars/DestroyStar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyStar : MonoBehaviour {
+
+    public float endTime = 3;
+
+    //IEnumerator EndStar()
+    //{
+    //    yield return new WaitForSeconds(endTime);
+    //}
+
+
+	void OnCollisionEnter ()
+    {
+        Destroy(gameObject, endTime);
+    }
+	
+}
