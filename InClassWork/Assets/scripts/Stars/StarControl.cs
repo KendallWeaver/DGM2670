9bc62a47646b8ca96010a00362b34c583e2dfc3a
Unknown <flaze5@gmail.com>
2016-10-26 13:54:17 -0600

    Oct 26

---
+++ InClassWork/Assets/scripts/Stars/StarControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarControl : MonoBehaviour {
+
+    public float endTime = 3;
+
+    public int forcetime = 2;
+
+    private bool canAddForce = true;
+    public float forceDuration = 0.1f;
+
+    private Rigidbody rigid;
+    private Vector3 forceVector;
+    private float force;
+    public float forceRange = 30;
+
+    private Vector3 tortVector;
+
+    //IEnumerator EndStar()
+    //{
+    //    yield return new WaitForSeconds(endTime);
+    //}
+    
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        StartCoroutine(RunRandomForce());
+    }
+
+    IEnumerator RunRandomForce()
+    {
+        force = Random.Range(-forceRange, forceRange);
+        while(forcetime > 0)
+        {
+
+            yield return new WaitForSeconds(forceDuration);
+            forceVector.x = force;
+            tortVector.z = force/force;
+            rigid.AddTorque(tortVector);
+            rigid.AddForce(forceVector);
+            forcetime--;
+        }
+    }
+
+	void OnCollisionEnter ()
+    {
+        Destroy(gameObject, endTime);
+    }
+	
+}
