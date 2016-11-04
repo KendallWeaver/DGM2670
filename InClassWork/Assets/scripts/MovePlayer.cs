55914d4afb668da9fe73b1bb67cd45630b6295eb
Unknown <flaze5@gmail.com>
2016-11-04 13:53:57 -0600

    Nov 4

---
+++ InClassWork/Assets/scripts/MovePlayer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+    /*
+     * 
+     * Attempted to get the code, but since the MoveUsingArrowKeys is incomplete, it won't run
+     * Try to problem solve on your own later
+    Vector3 moveDirection;
+    public float speed = 10;
+    public float gravity = 1;
+    public float jumpSpeed = 30;
+    CharacterController cc;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        cc = GetComponent<CharacterController>();
+        MoveUsingArrowKeys.MoveOnArrows = +Move;
+        MoveUsingArrowKeys.RotateOnArrows += Rotate ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Rotate(float _rotateInY)
+    {
+        transform.Rotate(0, _rotateInY, 0);
+    }
+
+    void Move(float _moveInZ)
+    {
+        if (cc.isGrounded)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed * Time.deltaTime;
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            moveDirection.x = _moveInZ * speed * Time.deltaTime;
+            moveDirection = transform.TransformDirection(moveDirection * Time.deltaTime);
+            cc.Move(MoveDirection);
+        }
+
+    }
+    */
+}
