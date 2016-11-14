b85b83a2dbf4af0cbf689bcda5808be45d4bad72
Unknown <flaze5@gmail.com>
2016-11-14 13:56:58 -0700

    Nov14 (End Of Class)

---
+++ InClassWork/Assets/scripts/Interfaces/AddPoints.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface AddPoints
+{
+
+    int points { get; set; }
+
+    void OnTriggerEnter();
+}
--- InClassWork/Assets/scripts/Interfaces/PowerUp.cs
+++ InClassWork/Assets/scripts/Interfaces/PowerUp.cs
@@ -5,9 +5,12 @@ using System.Collections;
 // Interface is like a list of requirements
 public interface PowerUp
 {
+    // get the power; set the power;
+    int power { get; set; }
 
 
+    void Start();
 
-
+    void OnTriggerEnter();
 
 }
