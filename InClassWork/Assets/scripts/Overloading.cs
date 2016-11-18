2c93697525838e83fae6514f26edd268a0caf85f
Unknown <flaze5@gmail.com>
2016-11-18 13:52:23 -0700

    Nov18 (Overloading done; going to inheritance)

--- InClassWork/Assets/scripts/Overloading.cs
+++ InClassWork/Assets/scripts/Overloading.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class Overloading : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+        Move("Fast");
 	}
+	
+    void Move(int speed)
+    {
+        Vector3 vector = new Vector3(speed, 0, 0);
+        transform.position = vector;
+    }
+
+    void Move (string speed)
+    {
+        Vector3 vector = Vector3.zero;
+
+        switch (speed)
+        {
+            case "Fast":
+                vector = new Vector3(100, 0, 0);
+                break;
+
+            case "Slow":
+                vector = new Vector3(1, 0, 0);
+                break;
+        }
+
+        transform.position = vector;
+    }
 }
---
+++ InClassWork/Assets/scripts/Parent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Walk();
+	}
+
+    void Walk()
+    {
+        print("I'M WALKING HERE");
+    }
+
+}
