9bd9defde09e434da955f4d4075477c0382f8c8a
Unknown <flaze5@gmail.com>
2016-11-30 13:23:28 -0700

    Nov 30 (first codes that are supposedly everwhere on the internet)

---
+++ InClassWork/Assets/scripts/Nov30/DelegateIntro.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateIntro : MonoBehaviour {
+
+    // this is what all the examples look like on the internet
+    public delegate void MyDelegate();
+    // events call delegates safely
+    public static MyDelegate MyEvent;
+
+	// Use this for initialization
+	void Start () {
+        MyEvent();
+	}
+	
+
+}
---
+++ InClassWork/Assets/scripts/Nov30/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        // function added to the event
+        DelegateIntro.MyEvent += MyEventHandler;
+	}
+
+    // function that................. i dunoo
+    private void MyEventHandler()
+    {
+        print("Ran");
+    }
+}
