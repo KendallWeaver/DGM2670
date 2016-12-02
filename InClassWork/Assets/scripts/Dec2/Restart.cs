ee619efa713eaf9d671cf73d7c7e3dc7ebcdc306
Unknown <flaze5@gmail.com>
2016-12-02 13:41:37 -0700

    Dec 2 (Assuming class is done...?)

--- InClassWork/Assets/scripts/Dec2/Restart.cs
+++ InClassWork/Assets/scripts/Dec2/Restart.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Restart : MonoBehaviour {
 
-    //public static Action ResetLevel;
+    public static Action ResetLevel;
 
 
 	// Use this for initialization
 	void Start () {
-        //endGame.GameOver += OnReset;
+       EndGame.GameOver += OnReset;
 	}
 	
 	// Update is called once per frame
 	void OnReset () {
-       // ResetLevel;	
+       ResetLevel();	
 	}
 }
