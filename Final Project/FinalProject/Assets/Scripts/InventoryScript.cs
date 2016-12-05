9fc5d65cf208faaff0340d839369bc2eb2b08564
Unknown <flaze5@gmail.com>
2016-12-05 13:51:24 -0700

    Dec 5 (Started working on Inventory Script)

---
+++ Final Project/FinalProject/Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryScript : MonoBehaviour {
+
+    //public string[] inventory = {"Stick", "Band-aid", "Donut"};
+    public List<string> theInventoryList = new List<string>();
+
+    void Start()
+    {
+        theInventoryList.Add("Stick");
+        theInventoryList.Add("Bandana");
+        theInventoryList.Add("Donut");
+    }
+    
+    /*
+    public List<string> PlayerInventory()
+    {
+        foreach (string item in theInventoryList)
+        {
+            return item;
+        }
+    }*/
+
+}
